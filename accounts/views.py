06c101e63921e61a4d502f76458e05bb73449369
kariss83 <vache.romain@gmail.com>
2022-08-17 21:06:34 +0200

    allows account registration but still have to work on redirections as all the urls are not set up yet

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,114 @@
-from django.shortcuts import render
-
 # Create your views here.
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from django.utils.http import urlsafe_base64_decode
+
+from accounts.forms import (
+	CustomUserCreationForm,
+	CustomAuthenticationForm,
+	CustomUserChangeForm
+)
+
+
+# login view
+def login_user(request):
+	# import pdb; pdb.set_trace()
+	if request.method == 'POST':
+		# import pdb; pdb.set_trace()
+		form = CustomAuthenticationForm(request.POST)
+		if form.is_valid():
+			email = request.POST.get('email', '')
+			password = request.POST.get('password', '')
+			user = authenticate(request, email=email, password=password)
+			if user is not None:
+				login(request, user)
+				messages.success(request, ('Vous êtes connecté(e)!'))
+				return redirect('/')
+			else:
+				messages.error(
+					request,
+					('Erreur de connexion - Veuillez reéssayer...')
+					)
+				return redirect('/accounts/login')
+		else:
+			messages.error(request, (
+				'Erreur de connexion - Veuillez reéssayer...'))
+			return redirect('/accounts/login')
+	else:
+		return render(
+			request,
+			'accounts/login.html',
+			{'form': CustomAuthenticationForm()}
+			)
+
+
+# logout view
+@login_required
+def logout_user(request):
+	logout(request)
+	messages.success(request, ('Vous êtes déconnecté(e)...'))
+	return redirect('/')
+
+
+# register view
+def register_user(request):
+	if request.method == 'POST':
+		form = CustomUserCreationForm(request.POST)
+		print(form.errors)
+		# import pdb; pdb.set_trace()
+		if form.is_valid():
+			email = form.cleaned_data['email']
+			password = form.cleaned_data['password1']
+			form.save()
+			user = authenticate(email=email, password=password)
+			# import pdb; pdb.set_trace()
+			if user is not None:
+				login(request, user)
+				messages.success(request, ('Vous êtes enregistré(e)...'))
+				return redirect('/accounts/profile')
+		else:
+			messages.error(request, (
+				'Erreur de Création de Comptes - Veuillez reéssayer...'))
+			return redirect('/accounts/register')
+	else:
+		form = CustomUserCreationForm()
+		context = {'form': form}
+		return render(
+			request,
+			'accounts/register.html',
+			context
+			)
+
+
+# profile view
+@login_required
+def profile(request):
+	return render(request, 'accounts/profile.html', {})
+
+	
+# edit profile view
+@login_required
+def edit_profile(request):
+	if request.method == 'POST':
+		# import pdb; pdb.set_trace()
+		form = CustomUserChangeForm(request.POST, instance=request.user)
+		if form.is_valid():
+			email = form.data['email']
+			name = form.data['name']
+			form.save()
+			messages.success(request, ('Modifications enregistrées avec succès.'))
+			return redirect('/accounts/profile')
+		else:
+			messages.error(request, (
+				'Erreur de modification des informations, le formulaire n\'est pas valide'))
+			return redirect('/accounts/edit')
+	else:
+		form = CustomUserChangeForm(instance= request.user)
+		context = {'form': form}
+		return render(
+			request,
+			'accounts/edit_profile.html',
+			context
+			)
--- config/settings.py
+++ config/settings.py
@@ -15,6 +15,16 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.1/howto/static-files/
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = 'static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
@@ -38,7 +48,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts.apps.AccountsConfig',
+    'crispy_forms'
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -55,7 +68,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -108,21 +121,26 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fr'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Paris'
 
 USE_I18N = True
 
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.1/howto/static-files/
-
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Custom user model
+
+AUTH_USER_MODEL = 'accounts.CustomUser'
+
+
+# Handling redirection from decorator @loginrequired
+LOGIN_URL = '/users/login/'
+LOGIN_REDIRECT_URL = '/users/login/'
