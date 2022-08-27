e749ed763577ba643170cfbe5536b8b3c5dc5b89
kariss83 <vache.romain@gmail.com>
2022-08-27 19:48:52 +0200

    handles the case where no barcode have been found on the image

--- barcode/views.py
+++ barcode/views.py
@@ -1,5 +1,8 @@
 import base64
-from django.shortcuts import render
+
+from django.shortcuts import render, redirect
+from django.contrib import messages
+
 
 from collection.models import Game
 from barcode.controllers.barcode_reader import ImageReader, Stringb64Reader
@@ -16,16 +19,20 @@ def upload_barcode(request):
             string_handler.string_to_PNG()
             barcode = string_handler.read_image()
 
-            gatherer = Gatherer(barcode)
-            name , img_url= gatherer.get_name_and_avg_price()
-            avg_price = gatherer.get_avg_price()
+            if len(barcode) == 0:
+                messages.error(request, ('No barcode detected - Try again...'))
+                return redirect('/barcode/upload')
+            else:
+                gatherer = Gatherer(barcode)
+                name , img_url= gatherer.get_name_and_avg_price()
+                avg_price = gatherer.get_avg_price()
 
-            game = Game.objects.get_or_create(barcode=barcode[0],
-                                              avg_price=avg_price,
-                                              name=name,
-                                              image=img_url,
-                                              )[0]
-            context = {'game': game}
+                game = Game.objects.get_or_create(barcode=barcode[0],
+                                                avg_price=avg_price,
+                                                name=name,
+                                                image=img_url,
+                                                )[0]
+                context = {'game': game}
 
         form = UploadFileForm(request.POST, request.FILES)
         if form.is_valid():
@@ -35,17 +42,21 @@ def upload_barcode(request):
             file_handler.handle_uploaded_file()
             barcode = file_handler.read_image()
 
-            gatherer = Gatherer(barcode)
-            name , img_url= gatherer.get_name_and_avg_price()
-            avg_price = gatherer.get_avg_price()
+            if len(barcode) == 0:
+                messages.error(request, ('No barcode detected - Try again...'))
+                return redirect('/barcode/upload')
+            else:
+                gatherer = Gatherer(barcode)
+                name , img_url= gatherer.get_name_and_avg_price()
+                avg_price = gatherer.get_avg_price()
 
-            game = Game.objects.get_or_create(barcode=barcode[0],
-                                              avg_price=avg_price,
-                                              name=name,
-                                              image=img_url,
-                                              )[0]
-            context = {'game': game,
-                    }
+                game = Game.objects.get_or_create(barcode=barcode[0],
+                                                avg_price=avg_price,
+                                                name=name,
+                                                image=img_url,
+                                                )[0]
+                context = {'game': game,
+                        }
         return render(request, 'barcode/upload.html', context)
     else:
         form = UploadFileForm()
