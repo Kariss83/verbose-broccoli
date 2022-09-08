f8f0f45b0eda66086d33aeffab9fea7ec166a373
kariss83 <vache.romain@gmail.com>
2022-09-08 18:45:02 +0200

    adds postgresql to ga to run tests

--- config/settings/github_action.py
+++ config/settings/github_action.py
@@ -3,9 +3,9 @@ from . import *
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'travis_ci_test',
-        'USER': 'postgres',
-        'PASSWORD': '',
+        'NAME': os.environ['POSTGRES_DB'],
+        'USER': os.environ['POSTGRES_USER'],
+        'PASSWORD': os.environ['POSTGRES_PASSWORD'],
         'HOST': '',
         'PORT': '5432',
     }
